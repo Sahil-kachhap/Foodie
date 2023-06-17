11b0365cb3f234a889d21e2b4a58e2f181b3e80c
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2023-06-17 13:18:33 +0530

    feat: added utility method to get imageUrl from imageId

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -4,3 +4,4 @@ const String databaseId = "646f62166f1fe65cde91";
 const String collectionId = "646f6265cd8aed1de94b";
 const String postCollectionId = "6477832e4c73ece2455a";
 const String postStorageId = "6473496b9748cefb57d4";
+String imageUrl(String imageId) => "https://cloud.appwrite.io/v1/storage/buckets/$postStorageId/files/$imageId/view?project=$appwriteProjectId&mode=admin";
